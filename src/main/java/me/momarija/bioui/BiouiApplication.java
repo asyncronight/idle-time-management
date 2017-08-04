1712b5edfd55ec533fe18c09871575bd3e3b6ab8
salah <salah.loukili@gmail.com>
2017-08-04 23:33:31 +0100

    Initial commit!

---
+++ src/main/java/me/momarija/bioui/BiouiApplication.java
@@ -0,0 +1,12 @@
+package me.momarija.bioui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiouiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiouiApplication.class, args);
+	}
+}
