6d12525c95f49ff876a50ecff9ad26e67b634725
salah <salah.loukili@gmail.com>
2017-08-05 04:10:51 +0100

    Added test for TraitementService to test the algorithm

---
+++ src/test/java/me/momarija/bioui/algo/TraitementServiceTest.java
@@ -0,0 +1,31 @@
+package me.momarija.bioui.algo;
+
+import me.momarija.bioui.repos.DonneeRepo;
+import me.momarija.bioui.services.TraitementService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TraitementServiceTest {
+
+	@Autowired
+	private DonneeRepo donneeRepo;
+
+	@Test
+	public void test(){
+		TraitementService serviceTest = new TraitementService();
+		Map<String,Integer> map = serviceTest.MyFunction(donneeRepo.findAll(),10,6,1.9f,0.7f);
+		assertEquals(map.size() , 3);
+		assertEquals( map.get("production"),new Integer(580));
+		assertEquals( map.get("ralenti"),new Integer(100));
+		assertEquals( map.get("arret"),new Integer(320));
+	}
+}
