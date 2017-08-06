6c7c2ef751d4fe6828a2c2cfadd3e2425583c9c1
salah <salah.loukili@gmail.com>
2017-08-06 01:21:31 +0100

    Added function to find engins of a particular chantier

--- src/main/java/me/momarija/bioui/repos/DonneeRepo.java
+++ src/main/java/me/momarija/bioui/repos/DonneeRepo.java
@@ -4,6 +4,10 @@ import me.momarija.bioui.domains.Donnee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface DonneeRepo extends JpaRepository<Donnee, Integer> {
+
+	List<Donnee> findByEnginId(int id);
 }
