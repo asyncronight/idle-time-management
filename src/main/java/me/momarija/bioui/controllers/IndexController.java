363d101a8ded028f9a1c9f91440687a703564aa8
salah <salah.loukili@gmail.com>
2017-08-08 18:32:22 +0100

    Demonstrating that retrieving data based on dates is working

--- src/main/java/me/momarija/bioui/controllers/IndexController.java
+++ src/main/java/me/momarija/bioui/controllers/IndexController.java
@@ -2,11 +2,13 @@ package me.momarija.bioui.controllers;
 
 import me.momarija.bioui.domains.Donnee;
 import me.momarija.bioui.repos.DonneeRepo;
+import org.apache.commons.lang3.time.DateUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.Date;
 import java.util.List;
 
 @Controller
@@ -15,9 +17,15 @@ public class IndexController {
 	@Autowired
 	private DonneeRepo donneeRepo;
 
+	/**
+	 * Returne les donnees + ou - une minute dés maintenant
+	 *
+	 */
 	@RequestMapping(value = "/")
 	@ResponseBody
 	public List<Donnee> index(){
-		return donneeRepo.findAll();
+		Date date = new Date();
+		return donneeRepo.findBetween(1, DateUtils.setMinutes(date,
+				date.getMinutes()-2),DateUtils.setMinutes(date,date.getMinutes()+2));
 	}
 }
