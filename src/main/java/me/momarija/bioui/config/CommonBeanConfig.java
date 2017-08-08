a0cac6318900d6965fcb22b60832e8f58eb1cfd4
salah <salah.loukili@gmail.com>
2017-08-08 17:42:50 +0100

    Added configs and filling sendDonnee method in DonneeFaker to simulate a data sent by arduino card

---
+++ src/main/java/me/momarija/bioui/config/CommonBeanConfig.java
@@ -0,0 +1,9 @@
+package me.momarija.bioui.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+public class CommonBeanConfig {
+}
