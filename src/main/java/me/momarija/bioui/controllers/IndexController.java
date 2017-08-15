2d6d18dd00a1d418a14f41b347fac709d8f08500
salah <salah.loukili@gmail.com>
2017-08-15 16:21:44 +0100

    1- added some pictures for index page 2- created a index controller to serve the index page.

--- src/main/java/me/momarija/bioui/controllers/IndexController.java
+++ src/main/java/me/momarija/bioui/controllers/IndexController.java
@@ -1,31 +1,15 @@
 package me.momarija.bioui.controllers;
 
-import me.momarija.bioui.domains.Donnee;
-import me.momarija.bioui.repos.DonneeRepo;
-import org.apache.commons.lang3.time.DateUtils;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import java.util.Date;
-import java.util.List;
 
 @Controller
 public class IndexController {
 
-	@Autowired
-	private DonneeRepo donneeRepo;
-
-	/**
-	 * Returne les donnees + ou - une minute dés maintenant
-	 *
-	 */
-	@RequestMapping(value = "/")
-	@ResponseBody
-	public List<Donnee> index(){
-		Date date = new Date();
-		return donneeRepo.findBetween(1, DateUtils.setMinutes(date,
-				date.getMinutes()-2),DateUtils.setMinutes(date,date.getMinutes()+2));
+	@RequestMapping(value = {"/", "/login" })
+	public String index(Model model){
+		model.addAttribute("title", "Bioui Analyzer");
+		return "index";
 	}
 }
