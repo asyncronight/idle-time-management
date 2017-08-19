f5d155379c1dc0835d52cb3fe70859a7cf9b535b
salah <salah.loukili@gmail.com>
2017-08-19 19:36:59 +0100

    1- Configuring file storage properties.. 2- Added a storage service to store and load files. 3- Setting up a end point to resolve filenames.

---
+++ src/main/java/me/momarija/bioui/controllers/FileResolver.java
@@ -0,0 +1,27 @@
+package me.momarija.bioui.controllers;
+
+import me.momarija.bioui.services.StorageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class FileResolver {
+
+	@Autowired
+	private StorageService storageService;
+
+	@GetMapping("/files/{filename:.+}")
+	@ResponseBody
+	public ResponseEntity<Resource> getFile(@PathVariable String filename) {
+		Resource file = storageService.loadFile(filename);
+		return ResponseEntity.ok()
+				.header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + file.getFilename() + "\"")
+				.body(file);
+	}
+}
---
+++ src/main/java/me/momarija/bioui/services/StorageService.java
@@ -0,0 +1,56 @@
+package me.momarija.bioui.services;
+
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.stereotype.Service;
+import org.springframework.util.FileSystemUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Date;
+
+@Service
+public class StorageService {
+
+	private final Path rootLocation = Paths.get("upload-dir");
+
+	public String store(MultipartFile file){
+		try {
+			String name= new Date().getTime()+file.getOriginalFilename();
+			Files.copy(file.getInputStream(), this.rootLocation.resolve(name));
+			return name;
+		} catch (Exception e) {
+			throw new RuntimeException("FAIL!");
+		}
+	}
+
+	public Resource loadFile(String filename) {
+		try {
+			Path file = rootLocation.resolve(filename);
+			Resource resource = new UrlResource(file.toUri());
+			if(resource.exists() || resource.isReadable()) {
+				return resource;
+			}else{
+				throw new RuntimeException("FAIL!");
+			}
+		} catch (MalformedURLException e) {
+			throw new RuntimeException("FAIL!");
+		}
+	}
+
+	public void deleteAll() {
+		FileSystemUtils.deleteRecursively(rootLocation.toFile());
+	}
+
+	public void init() {
+		try {
+			Files.createDirectory(rootLocation);
+		} catch (IOException e) {
+			throw new RuntimeException("Could not initialize storage!");
+		}
+	}
+}
