1ae84cc38935f101dc78f203a4349b1e440f7f72
salah <salah.loukili@gmail.com>
2017-08-22 20:18:32 +0100

    1- Added resources for doughnut chart (js) 2- Added doughnut chart for chantier's statistics and engin's statistics

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -10,3 +10,14 @@ $(function() {
 /*
 ########### Cercle chart #############
  */
+$(function() {
+    $('.circle-graph').easyPieChart({
+        scaleColor: false,
+        lineWidth: 20,
+        lineCap: 'butt',
+        barColor: '#a378aa',
+        trackColor: '#e7b8ef' ,
+        size: 150,
+        animate: 800
+    });
+});
