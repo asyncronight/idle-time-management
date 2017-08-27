fa5ad9a60b46ce1644e9a55269068b01f23cdeea
salah <salah.loukili@gmail.com>
2017-08-27 18:41:29 +0100

    1- Added StorageService class to store retrieve and delete files on disk. 2- Added an end point (FileResolver) to serve files.

--- src/main/java/me/momarija/bioui/config/CommonBeanConfig.java
+++ src/main/java/me/momarija/bioui/config/CommonBeanConfig.java
@@ -1,8 +1,17 @@
 package me.momarija.bioui.config;
 
+import me.momarija.bioui.service.StorageService;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class CommonBeanConfig {
 
+	@Bean
+	CommandLineRunner init(StorageService storageService) {
+		return (args) -> {
+			storageService.init();
+		};
+	}
 }
---
+++ src/main/java/me/momarija/bioui/service/StorageService.java
@@ -0,0 +1,66 @@
+package me.momarija.bioui.service;
+
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.stereotype.Service;
+import org.springframework.util.FileSystemUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Date;
+
+@Service
+public class StorageService {
+
+	private final Path rootLocation = Paths.get("upload-dir");
+
+	public String store(MultipartFile file){
+		try {
+			String name= new Date().getTime()+"";
+			Files.copy(file.getInputStream(), this.rootLocation.resolve(name));
+			return name;
+		} catch (Exception e) {
+			throw new RuntimeException("Enregistrement de la photo a échouée, veuillez réessayer.");
+		}
+	}
+
+	public Resource loadFile(String filename) {
+		try {
+			Path file = rootLocation.resolve(filename);
+			Resource resource = new UrlResource(file.toUri());
+			if(resource.exists() || resource.isReadable()) {
+				return resource;
+			}else{
+				throw new RuntimeException("La récuperation de la photo a échouée, veuillez réessayer.");
+			}
+		} catch (MalformedURLException e) {
+			throw new RuntimeException("La récuperation de la photo a échouée, verifier l'url.");
+		}
+	}
+
+	public void deleteAll() {
+		FileSystemUtils.deleteRecursively(rootLocation.toFile());
+	}
+
+	public void deleteFile(String filename){
+		try {
+			Files.deleteIfExists(rootLocation.resolve(filename));
+		} catch (IOException e) {
+			throw new RuntimeException("Operation échouée, impossible de supprimer la photo.");
+		}
+	}
+
+	public void init() {
+		try {
+			if (!Files.isDirectory(rootLocation))
+				Files.createDirectory(rootLocation);
+		} catch (IOException e) {
+			throw new RuntimeException("Could not initialize file storage!");
+		}
+	}
+
+}
