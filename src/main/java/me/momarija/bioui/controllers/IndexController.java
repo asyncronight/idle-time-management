5bf8d0939d87c181c936669edf5e211b343dbb41
salah <salah.loukili@gmail.com>
2017-09-04 15:37:33 +0100

    disable access to login page if user already logged in.

--- src/main/java/me/momarija/bioui/controllers/IndexController.java
+++ src/main/java/me/momarija/bioui/controllers/IndexController.java
@@ -4,11 +4,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.security.Principal;
+
 @Controller
 public class IndexController {
 
 	@RequestMapping(value = "/login")
-	public String index(Model model){
+	public String index(Model model, Principal principal) {
+		if (principal != null)
+			return "redirect:/";
 		model.addAttribute("title", "Bioui Analyzer");
 		return "login";
 	}
