faf2914d18f4bf2dc75cf611eaf2a7178961d9d3
salah <salah.loukili@gmail.com>
2017-09-09 14:05:59 +0100

    Fixed Chantier form errors. Add handler for uploading big images error.

--- src/main/java/me/momarija/bioui/controllers/ErrorHandling.java
+++ src/main/java/me/momarija/bioui/controllers/ErrorHandling.java
@@ -4,6 +4,7 @@ import org.springframework.core.annotation.AnnotationUtils;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.multipart.MultipartException;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
@@ -16,7 +17,11 @@ public class ErrorHandling {
 		if(AnnotationUtils.findAnnotation(ex.getClass(), ResponseStatus.class) != null)
 			throw ex;
 		ModelAndView mav = new ModelAndView();
-		mav.addObject("exception", ex);
+		if (ex instanceof MultipartException) {
+			mav.addObject("exception", new Exception("Image upload échouée, taille de l'image est plus que 1MB."));
+		} else {
+			mav.addObject("exception", ex);
+		}
 		mav.addObject("title", ex.getMessage());
 		mav.addObject("url", request.getRequestURL());
 		mav.setViewName("error/exception");
--- src/main/java/me/momarija/bioui/controllers/admin/AdminEnginController.java
+++ src/main/java/me/momarija/bioui/controllers/admin/AdminEnginController.java
@@ -2,14 +2,13 @@ package me.momarija.bioui.controllers.admin;
 
 import me.momarija.bioui.domains.Chantier;
 import me.momarija.bioui.domains.Engin;
-import me.momarija.bioui.service.AdminEnginService;
 import me.momarija.bioui.service.AdminChantierService;
+import me.momarija.bioui.service.AdminEnginService;
 import me.momarija.bioui.service.StorageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -56,6 +55,7 @@ public class AdminEnginController {
 							   @RequestParam(value = "file", required = false) MultipartFile file){
 		if (bindingResult.hasErrors()){
 			model.addAttribute("title", "Erreur");
+			model.addAttribute("chantier", adminCantierService.getChantier(idC));
 			return "admin/enginForm";
 		}
 		if (file.isEmpty())
