92ddaa68d330d526ea31ac7d71124a218a88bdf2
Walid Benjehd <walidbenjehd@gmail.com>
2017-09-09 17:02:24 +0100

    add date linked date picker to stats Form

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -9,3 +9,20 @@ function showDiv() {
            div.style.display = 'block';
        }
 }
+
+
+    $(function () {
+        $('#datetimepicker6').datetimepicker(({
+        	format: 'd/MM/YYYY, h:mm',
+        }));
+        $('#datetimepicker7').datetimepicker({
+        	format: 'd/MM/YYYY, h:mm',
+            useCurrent: false //Important! See issue #1075
+        });
+        $("#datetimepicker6").on("dp.change", function (e) {
+            $('#datetimepicker7').data("DateTimePicker").minDate(e.date);
+        });
+        $("#datetimepicker7").on("dp.change", function (e) {
+            $('#datetimepicker6').data("DateTimePicker").maxDate(e.date);
+        });
+    });
