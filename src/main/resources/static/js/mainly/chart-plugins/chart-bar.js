854b3ab86749e03f6762d6ff3b4bdefaa71d028d
kad <kadar.hamza@gmail.com>
2017-09-10 18:34:04 +0100

    Resolving chart-bar colors

--- src/main/resources/static/js/mainly/chart-plugins/chart-bar.js
+++ src/main/resources/static/js/mainly/chart-plugins/chart-bar.js
@@ -7,7 +7,21 @@ $(function() {
         'height' : percentage + '%'
         }, 1000);
 
+        if(percentage >=75){
+          $(this).css('background','#1ade6c');
 
+        }
+        else if(percentage >=50){
+          $(this).css('background','#1ade00');
 
+        }
+        else if(percentage>=25){
+          $(this).css('background','#c10000');
+
+        }else{
+          $(this).css('background','#ed0000');
+
+        }
   });
+
 });
